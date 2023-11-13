0886ede6154a6398568e2c33adc154df0e18a42b
christopher-distasio <stahzz1970@gmail.com>
2023-11-12 23:12:23 -0600

    Restores part 1 to its own branch

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
