162b83a2b053b4d16f56e233ae582e026a81bd10
Chris DiStasio <stahzz1970@gmail.com>
2023-11-15 17:25:07 -0600

    Adds error view

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ app.get('/', (req, res) => {
 app.use('/places', require('./controllers/places'))
 
 
-
 app.get('*', (req, res) => {
-    res.send('<h1>404 Page</h1>')
-})
-
+    res.render('error404')
+  })
+  
 app.listen(process.env.PORT)
