49562f958d3247400f9a00f8366f38be5bf3beeb
Chris DiStasio <stahzz1970@gmail.com>
2023-11-15 18:17:27 -0600

    Adds styles, images

--- views/default.jsx
+++ views/default.jsx
@@ -1,19 +1,18 @@
-const React = require("react");
+const React = require('react')
 
-function Def(html) {
-  return (
-    <html>
-      <head>
-        <title>Title</title>
-      </head>
-      <body>{html.children}</body>
-    </html>
-  );
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+                /* BOOTSTRAP CDN LINK (long) */
+                <link rel="stylesheet" href="/css/style.css"/>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
 }
 
-module.exports = Def;
-
-// function Def (html) {
-//     return (
-//     )
-//   }
+module.exports = Def
