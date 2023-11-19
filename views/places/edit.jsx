6d6c4a989e4b9b45776f911da7e7842ed47b76c5
Chris DiStasio <stahzz1970@gmail.com>
2023-11-19 15:08:08 -0600

    Show, edit routes working; all routes/views complete

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,65 +1,68 @@
 const React = require("react");
-const Def = require("../default.jsx");
+const Def = require("../default");
 
-function edit_form(data) {
-  return (
-    <Def>
-      <main>
-        <h1>Edit Place</h1>
-        <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
-          <div className="row">
-            <div className="form-group col-sm-6">
-              <label htmlFor="name">Place Name</label>
-              <input
-                className="form-control col-sm-6"
-                id="name"
-                name="name"
-                value={data.place.name}
-                required
-              />
-              <div className="form-group col-sm-6">
-                <label htmlFor="pic">Place Picture</label>
-                <input
-                  className="form-control col-sm-6"
-                  id="pic"
-                  name="pic"
-                  value={data.place.pic}
-                />
-              </div>
-              <div className="form-group col-sm-6" >
-                <label htmlFor="city">City</label>
-                <input
-                  className="form-control col-sm-6"
-                  id="city"
-                  name="city"
-                  value={city}
-                />
-              </div>
-              <div className="form-group col-sm-6">
-                <label htmlFor="state">State</label>
-                <input
-                  className="form-control col-sm-6"
-                  id="state"
-                  name="state"
-                  value={state}
-                />
-              </div>
-              <div className="form-group col-sm-6" >
-                <label htmlFor="cuisines">Cuisines</label>
-                <input
-                  className="form-control col-sm-6"
-                  id="cuisines"
-                  name="cuisines"
-                  value={cuisines}
-                  required
-                />
-              </div>
-            </div>
-          </div>
-        </form>
-      </main>
-    </Def>
-  );
+function edit_form({place, index}) {
+    return (
+        <Def>
+            <main>
+                <h1>Edit Place</h1>
+                <form method="POST" action={`/places/${index}?_method=PUT`}>
+                    <div className="row">
+                        <div className="form-group col-sm-6"></div>
+                        <label htmlFor="name">Place Name</label>
+                        <input
+                            className="form-control"
+                            id="name"
+                            name="name"
+                            value={place.name}
+                            required/>
+                    </div>
+                    <div className="form-group col-sm-6">
+                        <label htmlFor="pic">Place Picture</label>
+                        <input
+                            className="form-control"
+                            id="pic"
+                            name="pic"
+                            value={place.pic} 
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="city">City</label>
+                        <input
+                            className="form-control"
+                            id="city"
+                            name="city"
+                            value={place.city} 
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">State</label>
+                        <input
+                            className="form-control"
+                            id="state"
+                            name="state"
+                            value={place.state} 
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input
+                            className="form-control"
+                            id="cuisines"
+                            name="cuisines"
+                            value={place.cuisines} 
+                            required
+                        />
+                    </div>
+                    <input
+                        className="btn btn-primary"
+                        type="submit"
+                        value="Update Place"
+                    />
+                </form>
+            </main>
+        </Def>
+    );
 }
 
 module.exports = edit_form;
