7660d71a1900a5c5f0d2472f9bee77ebedac9d84
CHRISTOPHER DISTASIO <chris-d@CHRISTOPHERs-MacBook-Air.local>
2023-11-30 21:41:14 -0600

    Creates comment model

--- models/index.js
+++ models/index.js
@@ -3,4 +3,5 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI)
 
-module.exports.Place = require('./places')
+module.exports.Place = require('./place')
+module.exports.Comment = require('./comment')
