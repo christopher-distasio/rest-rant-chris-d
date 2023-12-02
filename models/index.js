9951fff0ad65bfa8ceeb0252201419e332e8dc72
Chris DiStasio <stahzz1970@gmail.com>
2023-12-01 21:21:50 -0600

    Creates, associates, seeds the comment model

--- models/index.js
+++ models/index.js
@@ -5,5 +5,6 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI)
 
 module.exports.Place = require('./places')
+module.exports.Comment = require('./comment')
 
 console.log(module.exports)
